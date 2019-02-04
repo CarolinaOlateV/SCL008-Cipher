14d55950e6cffc6c23b5156ccd292c28f5e757e3
carolinaolv <carolinaolatevallejos96@gmail.com>
2019-02-04 12:55:16 -0300

    INDEX.HTML agregando div para hacer 2 pagina, INDEX.JS conectando con INDEX.HTML, STYLE.CSS color a header y estilo de la letra

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,17 @@
 window.cipher = {
-  encode: () => {
+  //Cifrar
+  encode: (displace,capLetter) => {
     /* Acá va tu código */
-  },
-  decode: () => {
-    /* Acá va tu código */
+    let result = '';
+    for (let i=0; i<capLetter.length; i++){ //i=0 me devuelve la letra desde la posicion 0, i< me verifica 
+      let asciiNumber = capLetter.charCodeAt(i); //charcodeat devuelve la posicion de cada letra
+      if (asciiNumber >= 65 && asciiNumber <= 90 ) { //para saber si la letra mayúscula se encuentra en este rango
+        let convertedLetter=(asciiNumber-65+parseInt(displace))%26+65; //parse es para obtener el valor numerico del string
+  
+        result += String.fromCharCode(convertedLetter); //+=es para traerme la cadena completa
+      }
+    }
+    return result;
   }
-};
+  };
+  
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-/* Acá va tu código */
+document.getElementById('btnIngresar').addEventListener('click', (evento) =>{
+    evento.preventDefault();
+
+    document.getElementById('pagUno').style.display='none';
+    document.getElementById('pagDos').style.display='block';
+
+})
