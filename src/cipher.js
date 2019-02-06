5e9f9fd3fe8982c3346bd5b9e8c9d750107deb5e
carolinaolv <carolinaolatevallejos96@gmail.com>
2019-02-06 11:02:53 -0300

    INDEX.HTML agregando div para hacer 2 pagina, INDEX.JS conectando con INDEX.HTML, STYLE.CSS color a header y estilo de la letra

--- src/cipher.js
+++ src/cipher.js
@@ -1,2 +1,15 @@
-
+window.cipher = {
+    encode: (displace, text) => {
+        let result = '';
+        for(let i=0; i<text.length; i++){
+            let asciiNumber = text.charCodeAt(i);
+            if(asciiNumber >=65 && asciiNumber <=90){
+                let convertedLetter=(asciiNumber- 65 + parseInt(displace))%26 + 65;
+                result += String.fromCharCode(convertedLetter);
+            }  
+        }
+        return result;
+    }
+    
+};
   
--- src/index.js
+++ src/index.js
@@ -5,3 +5,13 @@ document.getElementById('btnIngresar').addEventListener('click', (evento) =>{
     document.getElementById('pagDos').style.display='block';
 
 })
+
+document.getElementById('btnEncode').addEventListener('click', () =>{
+    //evento.preventDefault();
+
+    let text = document.getElementById('mensaje').value;
+    let displace = document.getElementById('numero').value;
+
+    let result = window.cipher.encode(displace, text);
+    document.getElementById('resultado').innerHTML = result;
+})
