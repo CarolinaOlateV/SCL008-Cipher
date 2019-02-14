ea6a5ad0c581937d8a4eebd63c630bddf932b67e
carolinaolv <carolinaolatevallejos96@gmail.com>
2019-02-14 11:39:54 -0300

    CIPHER.JS agregando función de cifrado/descifrado para minúsculas

--- src/cipher.js
+++ src/cipher.js
@@ -1,19 +1,27 @@
 window.cipher = {
-    encode: (displace, text) => {
-        let result = '';
-        for(let i=0; i<text.length; i++){
-            let ascii = text.charCodeAt(i);
-            if(ascii >=65 && ascii <=90){
-                let converted=(ascii- 65 + parseInt(displace))%26 + 65;
-                result += String.fromCharCode(converted);
-            }  
-        else if (ascii === 32) {
-            result += ' ';
-        }
+    encode: (displace,text) => {
+      /* Acá va tu código */
+      let result = '';
+      for (let i=0; i<text.length; i++){ 
+        let ascii = text.charCodeAt(i); 
+        if (ascii >= 65 && ascii <= 90 ) { 
+          let converted=(ascii-65+parseInt(displace))%26+65;
+    
+          result += String.fromCharCode(converted); 
         
-    }
-    return result;
-},
+        }
+            else if (ascii >= 97 && ascii <= 122 ) {
+              let converted=(ascii-97+parseInt(displace))%26+97;
+    
+              result += String.fromCharCode(converted);
+            }else{
+              result += String.fromCharCode(ascii);
+      
+        }
+      }
+      return result;
+    },
+    
 
     decode: (displace, text) => {
         let result = '';
@@ -26,8 +34,14 @@ window.cipher = {
             }
             result += String.fromCharCode(converted);
         }
-        else if (ascii === 32) {
-            result += ' ';
+        else if (ascii >= 97 && ascii <= 122){
+            let converted=(ascii-97-parseInt(displace))%26+97;
+            if(converted <97)
+            converted +=26;
+
+            result += String.fromCharCode(converted);
+        }else{
+            result += String.fromCharCode(ascii);
         }
     }
     return result;
